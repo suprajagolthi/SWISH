2caf67135b1dbbc8c443f29509e19007f4515e12
Supraja_Golthi <36492833+suprajagolthi@users.noreply.github.com>
2019-08-10 13:47:59 +0530

    Update coffee.c

--- coffee.c
+++ coffee.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(void){
+  int d,c,t,cost=0;
+  int dist;
+  cost=cost+c;
+  scanf("%d,%d",&c,&d);
+    while(c>0){
+      dist=(d*c)/100;
+      c=c-dist;
+      cost=cost+c;
+    }
+  printf("%d",c);
+  return 0;
+}
 
